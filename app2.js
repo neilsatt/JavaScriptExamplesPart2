c5daf32dbd7bf924bda402351238c4370ab98762
Neil Satterfield <neilsatt@gmail.com>
2017-02-18 17:19:43 -0700

    add and remove elements

--- app2.js
+++ app2.js
@@ -5,6 +5,7 @@ const descriptionP = document.querySelector('p.description');
 const descriptionButton = document.querySelector('button.description');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
+const removeItemButton = document.querySelector('button.removeItemButton');
 
 
 toggleList.addEventListener('click', () => {
@@ -22,13 +23,20 @@ descriptionButton.addEventListener('click', () => {
     addItemInput.value = '';// clear the input box
 });
 
-// Adding elements to the DOM - appenChild method is added to the parent element
+// Adding elements to the DOM - appendChild method is added to the parent element
 addItemButton.addEventListener('click', () => {
     let ul = document.getElementsByTagName('ul')[0]; //returns a collection
     let li = document.createElement('li');
     li.textContent = addItemInput.value;
     ul.appendChild(li);
     addItemInput.value = '';// clear the input box
+});
+
+// Remove elements from the DOM - removeChild method is removed from the parent element
+removeItemButton.addEventListener('click', () => {
+    let ul = document.getElementsByTagName('ul')[0]; //returns a collection
+    let li = document.querySelector('li:last-child');
+    ul.removeChild(li);
 });
 
 
